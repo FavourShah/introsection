03acc4ffaec7972b1cc2cfaddb9899c4a11855fe
FavourShah <62761414+FavourShah@users.noreply.github.com>
2024-01-26 15:07:39 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+const hamburger = document.querySelector(".hamburger-btn");
+const mobileMenu = document.querySelector(".mobile-menu");
+const hamburgerImageOne = hamburger.querySelector(".active");
+const hamburgerImageTwo = hamburger.querySelector(".hidden");
+const mobileTitles = document.querySelectorAll(".mobile-menu-items li");
+const deskSubmenu = document.querySelectorAll(".list-items li");
+
+/* SIDEBAR AND HAMBURGER BUTTON TOGGLE */
+
+/* 
+hamburger.addEventListener('click', () => mobileMenu.classList.toggle('active')
+); */ // ALTERNATIVE WAY
+
+function mobileToggle() {
+  mobileMenu.classList.toggle("active");
+  hamburgerImageOne.classList.toggle("hidden");
+  hamburgerImageTwo.classList.toggle("hidden");
+}
+
+hamburger.addEventListener("click", mobileToggle);
+
+/* MOBILE MENU DROPDOWN SUBMENU TOGGLE */
+
+mobileTitles.forEach((mobileTitle) => {
+  mobileTitle.addEventListener("click", () => {
+    let isActive = mobileTitle.classList.contains("active");
+    /* 
+        mobileTitles.forEach(el => {
+                el.classList.remove('active');  // removes the active state from one list item once another is clicked
+            })  */
+
+    if (isActive) mobileTitle.classList.remove("active");
+    else mobileTitle.classList.add("active");
+  });
+});
+
+/*    DESKTOP MENU DROPDOWN TOGGLE */
+
+deskSubmenu.forEach((deskSub) => {
+  deskSub.addEventListener("click", () => {
+    let open = deskSub.classList.contains("active");
+
+    if (open) {
+      deskSub.classList.remove("active");
+    } else {
+      deskSub.classList.add("active");
+    }
+  });
+});
